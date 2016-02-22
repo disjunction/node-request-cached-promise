26607d86056b8baa9d618301cc31674ff99bc814
or <or@pluseq.com>
2016-02-23 00:41:38 +0100

    finish draft of testMongooseCache.js

--- test/testMongooseCache.js
+++ test/testMongooseCache.js
@@ -10,30 +10,30 @@ let requestCached = require("../src/index"),
 function defineTest() {
     describe("request-cached-promise with mongoose", () => {
         it ("works", function(done) {
-            console.log('hello')
+            this.timeout(1000);
+
             nock("http://example.com")
                 .get("/test-path")
                 .reply(200, "hello foo");
+
             let cache = cm.caching({
                 store: cmm,
-                uri: "mongodb://127.0.0.1/test",
-                options: {
-                    collection: "my_test",
-                    ttl: 60000
+                mongoose: mongoose,
+                modelOptions: {
+                    collection: "cacheman_rcp"
                 }
             });
 
+            mongoose.connect("mongodb://127.0.0.1/test");
+
             let request = requestCached.bindToCache(cache);
+
             Promise.resolve()
-                .then(() => {
-                    // give it some time to connect (lol)
-                    return new Promise(resolve => setTimeout(resolve, 1000));
-                })
                 .then(() => {
                     return request("http://example.com/test-path");
                 })
                 .then(body => {
-                    expect(body).to.equal("hello foo"); 
+                    expect(body).to.equal("hello foo");
                     // this request would not succeed without caching, because nock works only once
                     return request("http://example.com/test-path");
                 })
